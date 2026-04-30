7bde823b756ba5d0e88a1933b282f1c56823d46a
jjcoding-source <jebinjacob62@gmail.com>
2026-04-30 13:34:32 +0530

    Add UpdatePreferencesRequest DTO for managing user notification preferences

---
+++ RentEase.API/DTOs/User/UpdatePreferencesRequest.cs
@@ -0,0 +1,10 @@
+﻿namespace RentEase.API.DTOs.User
+{
+    public class UpdatePreferencesRequest
+    {
+        public bool PrefBookingUpdates { get; set; }
+        public bool PrefNewListings { get; set; }
+        public bool PrefPriceDropAlerts { get; set; }
+        public bool PrefMarketingEmails { get; set; }
+    }
+}
