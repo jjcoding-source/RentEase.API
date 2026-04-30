a86b5c083ef85a53d1b15a5fb5d4f55d258dc716
jjcoding-source <jebinjacob62@gmail.com>
2026-04-30 13:31:39 +0530

    Add BookingRequest DTO with validation for creating booking requests

---
+++ RentEase.API/DTOs/Booking/BookingRequest.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace RentEase.API.DTOs.Booking
+{
+    public class BookingRequest
+    {
+        [Required] public int PropertyId { get; set; }
+        [Required] public DateTime MoveIn { get; set; }
+        public DateTime? MoveOut { get; set; }
+        [Required] public int Duration { get; set; }
+        public string Occupants { get; set; } = "1 person";
+        public string Parking { get; set; } = "No";
+        public string? Message { get; set; }
+    }
+}
